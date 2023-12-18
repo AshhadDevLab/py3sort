566e7dd2650f5503cc4b3ef08654dfdaa94eb25c
Ashhad776 <ashhadahmed776@outlook.com>
2023-12-19 01:23:57 +0500

    Initial commit: Package setup ⚙️
    Version value fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='py3sort',
-    version='1.0.2',
+    version='0.1.8',
     packages=find_packages(),
     install_requires=[],
     author='Ashhad Ahmed',
